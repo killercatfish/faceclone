05481bbd48271860de849f2a583be952286fe6bc
Josh Gold <jgold@NVTHS1.PRVT>
2019-01-17 12:04:53 -0800

    first commit

---
+++ faceclone-part-2/template.php
@@ -0,0 +1,9 @@
+<?php include header.php” ?>
+
+  <!-- main -->
+  <main class="container">
+    
+  </main>
+  <!-- ./main -->
+
+<?php include “footer.php” ?>
---
+++ faceclone-part-3/template.php
@@ -0,0 +1,13 @@
+<?php require_once "functions.php" ?>
+<?php include "header.php" ?>
+
+<?php 
+  db_connect();
+?>
+  <!-- main -->
+  <main class="container">
+    
+  </main>
+  <!-- ./main -->
+
+<?php include “footer.php” ?>
